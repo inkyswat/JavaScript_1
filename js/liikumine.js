3435302d4ffbdc276298688cdd48adc5fe5bc947
Oliver Roots <inkyswat@tlu.ee>
2018-03-22 17:20:50 +0200

    refresh, liikumine, pallid liiikuma

---
+++ js/liikumine.js
@@ -0,0 +1,60 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+var ruut = {
+    x: 30,
+    y: 9.8,
+    w: 20,
+    h: 20,
+    kiirus: 0
+}
+
+var ruut2 = {
+    x: 50,
+    y: 9.8,
+    w: 20,
+    h: 20,
+    kiirus: 4
+}
+
+// canvase refreshimine
+
+var gravity = .96;
+var bounce = 0.8;
+var bounce_2 = 0.7;
+
+    function arvuta () {
+        ruut.kiirus += gravity;
+        ruut.y += ruut.kiirus;
+
+        if( (ruut.y + ruut.h) > canvas.height) {
+            ruut.y = canvas.height - ruut.h;
+            ruut.kiirus = -ruut.kiirus * bounce;
+        }
+
+    }
+
+    function arvuta_2() {
+    ruut2.kiirus += gravity;
+    ruut2.y += ruut2.kiirus;
+
+    if ((ruut2.y + ruut2.h) > canvas.height) {
+        ruut2.y = canvas.height - ruut2.h;
+        ruut2.kiirus = -ruut2.kiirus * bounce_2;
+    }
+
+}
+
+    function joonista() {
+            console.log(joonista);
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.fillRect(ruut.x, ruut.y, ruut.w, ruut.h);
+            ctx.fillRect(ruut2.x, ruut2.y, ruut2.w, ruut2.h);
+            arvuta();
+            arvuta_2();
+            requestAnimationFrame(joonista); // refreshimine jääb seisma kui mujale tab'i peale minna
+        }
+
+
+        
+requestAnimationFrame(joonista);
---
+++ js/liikumine_2.js
@@ -0,0 +1,56 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var canvasLocation = canvas.getBoundingClientRect();
+
+ 
+    function joonista() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        // console.log(joonista);
+        for (var i = 0; i < pallid.length; i++) { // pallid.length loeb palju elemente on pallid massiivis
+        ctx.beginPath();
+        ctx.arc(pallid[i].x, pallid[i].y, pallid[i].r, 0, 2 * Math.PI);
+        ctx.fill();
+
+        }
+    requestAnimationFrame(joonista); // refreshimine jääb seisma kui mujale tab'i peale minna
+    }
+
+    function Pall(x,y,r) { // palli objekti loomise funktsioon
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+    
+//    var pall1 = new Pall(20,50,30); 
+   var pallid = [];
+   for(var i = 0; i < 10; i++) {
+       var r = parseInt(10 + Math.random() * 30);
+       var x = parseInt(r + Math.random() * (canvas.width - 2*r)); // et pall tekiks canvase alass on vaja
+       var y = parseInt(r + Math.random() * (canvas.height - 2*r));
+       pallid.push(new Pall(x,y,r));
+   }     
+requestAnimationFrame(joonista);
+
+window.addEventListener("mousedown", function(e) {
+    var hx = e.clientX - canvasLocation.x;
+    var hy = e.clientY - canvasLocation.y;
+
+    for (var i = 0; i < pallid.length; i++) {
+        var kaugus_x = pallid[i].x - hx;
+        var kaugus_y = pallid[i].y - hy;
+        var kaugus = Math.sqrt(kaugus_x * kaugus_x + kaugus_y * kaugus_y) - pallid[i].r;
+        if (kaugus <= 0) {
+            // pallid[i].r += 0.1*pallid[i].r;
+            // i++;
+            // pallid.splice(i, 1); // alustab kustutamist, ja kui palju
+
+            pallid[i].x += kaugus_x;
+            pallid[i].y += kaugus_y;
+            kiirus_x = kaugus_x;
+            kiirus_y = kaugus_y;
+        }
+
+    }
+
+});
+
