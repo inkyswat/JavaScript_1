abadf78f572dd8bc597c90e00ed7cd2eec285c49
Oliver Roots <inkyswat@tlu.ee>
2018-03-31 00:01:43 +0300

    uus katsetus liikumine(exp), kasti sujuvalt peatama saamine, y=1/x

--- js/liikumine(exp).js
+++ js/liikumine(exp).js
@@ -6,17 +6,16 @@ var ruut = {
     y: 9.8,
     w: 20,
     h: 20,
-    y_kiirus: -8,
-    x_kiirus: 15,
-    suund: 1,
-    slide: 10
+    y_kiirus: 0,
+    x_kiirus: 10,
+    suund: 1
 }
 
 var ruut2 = {
     x: 60,
     y: 500,
-    w: 25,
-    h: 25,
+    w: 50,
+    h: 60,
     y_kiirus: -32,
     x_kiirus: 10,
     suund: 1
@@ -25,25 +24,39 @@ var ruut2 = {
 // canvase refreshimine
 
 var gravity = .98;
-var bounce = 0.9;
-var bounce_2 = 0.5;
-var counter = 2;
+var bounce = 0.8;
+var bounce_2 = 0.6;
+var counter = 10;
     function arvuta () {
         ruut.y_kiirus = precisionRound(ruut.y_kiirus, 2);
         ruut.y_kiirus += gravity;
         ruut.y += ruut.y_kiirus;
         ruut.x += ruut.x_kiirus;
-        ruut.x = precisionRound(ruut.x, 0);
-        ruut.y = precisionRound(ruut.y, 0);
+        // ruut.x = precisionRound(ruut.x, 0);
+        // ruut.y = precisionRound(ruut.y, 0);
         // ruut.x += ruut.x_kiirus;
-        ruut.x_kiirus = precisionRound(ruut.x_kiirus, 0);
+        // ruut.x_kiirus = precisionRound(ruut.x_kiirus, 0);
+
+        if (ruut.x_kiirus > 1) {
+          counter = counter + 0.1;
+          ruut.x_kiirus *= ((1/(counter))+0.92);
+          // ruut.x_kiirus *= (-0.2*counter+1);
+        }
+        else {
+          // ruut.x_kiirus = 0;
+        }
+        // ruut.x_kiirus -= ruut.x_kiirus;
+        // console.log(ruut.x_kiirus);
+
+
 
         if ((ruut.y + ruut.h) > canvas.height) {
             ruut.y = canvas.height - ruut.h;
             ruut.y_kiirus = -ruut.y_kiirus * bounce;
-            ruut.y_kiirus = precisionRound(ruut.y_kiirus, 2);
+            // ruut.y_kiirus = precisionRound(ruut.y_kiirus, 2);
 
         }
+
         // kui kiirus on piisavalt kaua iteratsioone olnud väikseimas vahemikus siis liikumine peatub, JÄRSULT
         // vaja sujuvat peatumist (näiteks y=1/x rakendust)
         //
@@ -54,23 +67,23 @@ var counter = 2;
         //   }
         // }
         if ((ruut.x + ruut.h) >= canvas.width) {
-            counter = counter + 0.4;
-            ruut.x -= 1;
-            ruut.suund *= -1;
-            ruut.x_kiirus *= ruut.suund;
-            ruut.x_kiirus *= (1/(counter+0.6)+0.5);
-            // ruut.x_kiirus = precisionRound(ruut.x_kiirus, 1);
-        }
-        if (ruut.x <= 1) {
-            counter = counter + 0.4;
-            ruut.x += 1;
-            ruut.suund *= -1;
-            ruut.x_kiirus *= ruut.suund;
-            ruut.x_kiirus *= (1/(counter+0.6)+0.5);
-            // ruut.x_kiirus = precisionRound(ruut.x_kiirus, 1);
+                ruut.suund *= -1;
+                if((ruut.x + ruut.x_kiirus) > canvas.width) {
+                  ruut.x = canvas.width - 1;
+                  ruut.x_kiirus *= ruut.suund;
+                }
 
         }
+        if (ruut.x < 1) {
+            // ruut.x += 1;
+                ruut.suund *= -1;
+                ruut.x_kiirus *= ruut.suund;
 
+                if((ruut.x + ruut.x_kiirus) < 0) {
+                  ruut.x = 0;
+                }
+
+        }
         // if (ruut.y_kiirus < 0.1) {
         //     ruut.x_kiirus *= ruut.suund;
         // }
@@ -88,8 +101,8 @@ var counter = 2;
 }
 
     function joonista() {
-            // console.log(ruut.slide);
-            console.log(ruut.x);
+            // console.log(ruut.x_kiirus);
+            console.log(ruut.x_kiirus);
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             ctx.fillRect(ruut.x, ruut.y, ruut.w, ruut.h);
             ctx.fillRect(ruut2.x, ruut2.y, ruut2.w, ruut2.h);
