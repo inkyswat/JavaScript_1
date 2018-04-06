2aae1168071edcb36742ef9975a3b3715af47c84
Oliver Roots <inkyswat@tlu.ee>
2018-04-06 12:31:36 +0300

    mõned muutujad ümber nimetatud, koodi puhastamine

--- js/object_method.js
+++ js/object_method.js
@@ -1,51 +1,32 @@
 
 class wheel {
-    constructor (posX, posY, MoveSpeed, SpeedLimit, radius, radiusChange, direction, Accel) {
+    constructor (posX, posY, MoveSpeed, radius, radiusChange, direction, Accel) {
 
     this.posX = posX;
     this.posY = posY;
     this.MoveSpeed = MoveSpeed;
-    this.SpeedLimit = SpeedLimit;
     this.radius = radius;
     this.radiusChange = radiusChange;
     this.direction = direction;
     this.Accel = Accel;
     this.ctx = getCanvas();
     }
-    // getPosX() {
-    //     return this.posX; //klassist andmete kätte saamine
-    // }
-    // getPosY() {
-    //     return this.posY; //klassist andmete kätte saamine
-    // }
-    // getRadius() {
-    //     return this.radius; //klassist andmete kätte saamine
-    // }
-
-
-    move (direction = this.direction) {
-        // console.log(this.MoveSpeed);
+    manipul (direction = this.direction) {
         
         if (this.radius > 1 && this.radius < 500 ) {
-      //      console.log(this.MoveSpeed);
             this.radius += this.radiusChange; // radius ei tohi olla negatiivne, siis peksab seghašt!
             this.MoveSpeed += this.Accel;
-
-// console.log(direction);
 
             if (this.radius < 1) { // paneme raadiuse nulliks kui tahab negatiivseks minna
                 this.radius = 0;
             }
             else if (this.direction > 0 && this.direction < 90 || this.direction > 270 && this.direction < 360) {
-
-
                 this.posY += this.MoveSpeed * Math.cos(this.direction);
                 this.posX += this.MoveSpeed * Math.sin(this.direction);
             }
             else if (this.direction > 90 && this.direction < 180 || this.direction > 180 && this.direction < 270) {
                 this.posY += this.MoveSpeed * Math.cos(this.direction);
                 this.posX += this.MoveSpeed * Math.sin(this.direction);
-
             }
             else if (direction == 0 || direction == 360) {
                 this.posX += this.MoveSpeed;
@@ -86,7 +67,7 @@ function joonista() {
  for (let i = 0; i < wheels.length; i++) {
     
     wheels[i].draw(wheels[i].posX, wheels[i].posY, wheels[i].radius);
-    wheels[i].move();
+    wheels[i].manipul();
  }
    
     requestAnimationFrame(joonista); // refreshimine jääb seisma kui mujale tab'i peale minna
@@ -95,21 +76,17 @@ function joonista() {
 var ctx = getCanvas();
 wheel.iterations = 1;
 var wheels = [];
-for (let i = 0; i < 1500; i++) {
+for (let i = 0; i < 700; i++) {
     let r = parseInt(10 + Math.random() * 10);
     let x = parseInt(r + Math.random() * (canvas.width - 2 * r)); // et pall tekiks canvase alass on vaja ruudu laius maha lahutada
     let y = parseInt(r + Math.random() * (canvas.height - 2 * r));
-    let MoveSpeed = 1.1 + parseInt(Math.random()*50);
+    let MoveSpeed = 1.2+parseInt(Math.random()*4);
     let direction = 360*(Math.random());
     let Accel = Math.random()*0.022-0.034;
-    let radiusChange = Math.random() * 1.1 - 1;
-    wheels.push(new wheel(350, 300, MoveSpeed, 1, r, radiusChange , direction, Accel));
+    let radiusChange = Math.random() * 1.3 - 1.25;
+    wheels.push(new wheel(350, 300, MoveSpeed, r, radiusChange , direction, Accel));
 }
 // var wheel1 = new wheel(100, 100, 5, 1, 30, 1.1, 270, -0.05);
-    // for (let index = 0; index < wheels.length; index++) {
-    //     console.log(wheels[index].direction);
-    // }
-        
 requestAnimationFrame(joonista);
 
 function time(milliseconds) {
